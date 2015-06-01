4c1bf363bf05f2362b662ac86ed9ccfba4e684cf
Ankur <aksinghmt@thoughtworks.com>
2015-06-01 19:39:33 +0530

    adding images folder and putting some jpg images to it, in app.js changing gif to jpg

--- cl_8/app.js
+++ cl_8/app.js
@@ -23,9 +23,9 @@
     color: '#EEE',
     faces: 12,
     images: [
-      "images/gem-01.gif",
-      "images/gem-03.gif",
-      "images/gem-04.gif"
+      "images/gem-01.jpg",
+      "images/gem-03.jpg",
+      "images/gem-04.jpg"
     ]
   }, {
     name: 'Zircon',
@@ -36,9 +36,9 @@
     color: '#000',
     faces: 6,
     images: [
-      "images/gem-06.gif",
-      "images/gem-07.gif",
-      "images/gem-09.gif"
+      "images/gem-06.jpg",
+      "images/gem-07.jpg",
+      "images/gem-09.jpg"
     ]
   }];
 })();
